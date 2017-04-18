14b3a8da64babf6f9f9a0931cd84bda11ce37e47
Ki Song <ki@audioverse.org>
2017-04-18 18:01:13 -0400

    Update

---
+++ example-local-define.php
@@ -0,0 +1,5 @@
+<?php
+
+define( 'OAUTH2__AUTHORIZE_URI', 'https://www.audioverse.org.lan/oauth/authorize' );
+define( 'OAUTH2__CLIENT_ID', 12 );
+define( 'OUATH2__API_USER_URI', 'http://localhost:8000/auth/user');
--- wp-avorg-oauth-plugin.php
+++ wp-avorg-oauth-plugin.php
@@ -7,9 +7,7 @@
  * Author: Ki Song
  */
 
-define( 'OAUTH2__AUTHORIZE_URI', 'https://www.audioverse.org.lan/oauth/authorize' );
-define( 'OAUTH2__CLIENT_ID', 12 );
-define( 'OUATH2__API_USER_URI', 'http://localhost:8000/auth/user');
+include 'local-define.php';
 
 class AVOauth2 {
 
