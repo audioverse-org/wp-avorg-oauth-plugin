6ba41836caf31f6858deb30c21fb1cb8aac7e891
Ki Song <ki@audioverse.org>
2017-04-18 17:54:17 -0400

    Inital commit

---
+++ wp-avorg-oauth-plugin.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Plugin Name: AVORG OAUTH PLUGIN
+ * Plugin URI: https://www.audioverse.org
+ * Description: Adds OAUTH2 login to WordPress that works well with Laravel Passport
+ * Version 1.0
+ * Author: Ki Song
+ */
+
+define( 'OAUTH2__AUTHORIZE_URI', 'https://www.audioverse.org.lan/oauth/authorize' );
+define( 'OAUTH2__CLIENT_ID', 12 );
+define( 'OUATH2__API_USER_URI', 'http://localhost:8000/auth/user');
+
+class AVOauth2 {
+
+    protected $token = false;
+    protected $userObj;
+    protected $api_response;
+
+    function __construct()
+    {
+        if (isset($_GET['token'])) {
+            $this->token = 'Bearer ' . $_GET['token'];
+        }
+    }
+
+    static function &init() {
+
+        static $instance = false;
+        if ( !$instance ) {
+            $instance = new AVOauth2;
+        }
+        return $instance;
+    }
+
+    /**
+     * Attempt to login. If username doesn't exist, create the user, login
+     * user with only using username
+     */
+    private function login() {
+
+        if ( !is_user_logged_in() && $this->loadUserData() ) {
+
+            if (!username_exists($this->userObj->email)) {
+                $password = wp_generate_password(12, true);
+                $user_id = wp_create_user($this->userObj->email, $password, $this->userObj->email);
+                wp_update_user(
+                    array(
+                        'ID' => $user_id,
+                        'nickname' => $this->userObj->email
+                    )
+                );
+            }
+
+            // login user
+            $user = get_user_by('login', $this->userObj->email);
+
+            if (!is_wp_error($user)) {
+                wp_clear_auth_cookie();
+                wp_set_current_user($user->ID);
+                wp_set_auth_cookie($user->ID);
+                // redirect to avoid showing token on url
+                $url_parts = parse_url($_SERVER['REQUEST_URI']);
+                wp_redirect($url_parts['path']);
+                exit;
+            }
+        } // end is user_logged_in and load userData function
+    } // end login method
+
+    private function loadUserData() {
+
+        if ( $this->token !== false) {
+
+            $this->api_response = wp_remote_get(OUATH2__API_USER_URI, [
+                'headers' => [
+                    'Authorization' => $this->token
+                ]
+            ]);
+
+            $response_obj = json_decode($this->api_response['body']);
+
+            if (!isset($response_obj->status_code)) {
+                if ( isset($response_obj->data->email) ) {
+                    $this->userObj = $response_obj->data;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static function plugins_loaded() {
+
+        $avOuath2 = AVOauth2::init();
+        // try to login
+        $avOuath2->login();
+    }
+
+    public static function login_url() {
+        $return_uri = urlencode($_SERVER['REQUEST_URI']);
+        return '<a href="'.OAUTH2__AUTHORIZE_URI.'?client_id='.OAUTH2__CLIENT_ID.'&return_uri='.$return_uri.'">Login</a>';
+    }
+
+    public static function logout_url() {
+
+        return '<a href="'.$_SERVER['REQUEST_URI'].'?oauth2_logout">Logout</a>';
+    }
+}
+
+add_shortcode( 'oauth2_login_url', array('AVOauth2', 'login_url'));
+add_shortcode( 'oauth2_logout', array('AVOauth2', 'logout_url'));
+add_action( 'plugins_loaded', array( 'AVOauth2', 'plugins_loaded' ));
